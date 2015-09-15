1d810576c29651af79ce55537d3e77e2573390a1
MatsumuraYuki <define1394@gmail.com>
2015-09-15 15:11:01 +0900

    support robolectric test

---
+++ app/src/test/java/jp/co/tweetmap/CustomRobolectricTestRunner.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015 TweetMap All Right Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jp.co.tweetmap;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.RobolectricTestRunner;
+
+public class CustomRobolectricTestRunner extends RobolectricTestRunner {
+  public CustomRobolectricTestRunner(Class<?> testClass) throws InitializationError {
+    super(testClass);
+    String buildVariant = (BuildConfig.FLAVOR.isEmpty() ? "" : BuildConfig.FLAVOR + "/") + BuildConfig.BUILD_TYPE;
+    System.setProperty("android.package", BuildConfig.APPLICATION_ID);
+    System.setProperty("android.manifest", "build/intermediates/manifests/full/" + buildVariant + "/AndroidManifest.xml");
+    System.setProperty("android.resources", "build/intermediates/res/" + buildVariant);
+    System.setProperty("android.assets", "build/intermediates/assets/" + buildVariant);
+  }
+}
